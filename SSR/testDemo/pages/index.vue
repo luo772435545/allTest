4218414f50bbef215d2295a5815892cdfa942c61
luo772435545 <772435545@qq.com>
2018-03-16 09:31:59 +0800

    01

--- SSR/testDemo/pages/index.vue
+++ SSR/testDemo/pages/index.vue
@@ -1,23 +1,9 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        testdemo
+        {{message}}
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
@@ -26,6 +12,11 @@
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
+    data(){
+        return{
+            message:'hello'
+        }
+    },
   components: {
     AppLogo
   }
